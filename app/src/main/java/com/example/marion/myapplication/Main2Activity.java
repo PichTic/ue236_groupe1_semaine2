ab721738cdfd8251d70766b790331bb81e009788
Noiramir <shadowyzz@hotmail.fr>
2017-11-01 12:11:40 +0100

    Première étape projet
    Fichier android studio de la première étape

---
+++ app/src/main/java/com/example/marion/myapplication/Main2Activity.java
@@ -0,0 +1,28 @@
+package com.example.marion.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void creerToast (View view) {
+            Toast.makeText(getApplicationContext(), "Le bouton de validation a été cliqué.", Toast.LENGTH_SHORT).show();
+        Intent startOldActivity = new Intent(this, MainActivity.class);
+        startActivity(startOldActivity);
+    }
+
+    public void retourActivite (View view) {
+        Intent backToOldActivity = new Intent(this, MainActivity.class);
+        startActivity(backToOldActivity);
+    }
+
+}

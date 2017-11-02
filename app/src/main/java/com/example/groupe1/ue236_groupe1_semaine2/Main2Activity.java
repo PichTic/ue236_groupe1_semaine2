0d793730ac371804aad6d9570f5290dfd77705dd
PichTic <pichot.vincent@gmail.com>
2017-11-02 08:40:41 +0100

    Added Step 3 First draft

--- app/src/main/java/com/example/groupe1/ue236_groupe1_semaine2/Main2Activity.java
+++ app/src/main/java/com/example/groupe1/ue236_groupe1_semaine2/Main2Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 //import com.example.groupe1.ue236_groupe1_semaine2.R;
@@ -14,6 +15,17 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView showResult = (TextView) findViewById(R.id.textView);
+
+        Intent intentResult = this.getIntent();
+
+        String Vincent = intentResult.getStringExtra("V");
+        String Melissa = intentResult.getStringExtra("M");
+        String Emmanuel = intentResult.getStringExtra("Me");
+        String Marion = intentResult.getStringExtra("Em");
+
+        showResult.setText(Vincent+" , "+Melissa+" , "+Emmanuel+" , "+Marion);
     }
 
     public void creerToast (View view) {
--- app/src/main/java/com/example/groupe1/ue236_groupe1_semaine2/MainActivity.java
+++ app/src/main/java/com/example/groupe1/ue236_groupe1_semaine2/MainActivity.java
@@ -4,10 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 //import com.example.groupe1.ue236_groupe1_semaine2.R;
 
 public class MainActivity extends AppCompatActivity {
+
+   // Intent intent;
+    String Vincent;
+    String Melissa;
+    String Marion;
+    String Emmanuel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +24,52 @@ public class MainActivity extends AppCompatActivity {
 
     public void nouvelleActivite (View view) {
         Intent startNewActivity = new Intent(this, Main2Activity.class);
+        startNewActivity.putExtra("V", Vincent);
+        startNewActivity.putExtra("M", Marion);
+        startNewActivity.putExtra("Me", Melissa);
+        startNewActivity.putExtra("Em", Emmanuel);
         startActivity(startNewActivity);
     }
 
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch (view.getId()) {
+
+            case R.id.checkBox:
+                if (checked) {
+                    Marion = "Marion";
+                } else {
+                    Marion = "";
+                }
+                break;
+
+            case R.id.checkBox2:
+                if (checked) {
+                    Vincent = "Vincent";
+                } else {
+                    Vincent = "";
+                }
+                break;
+            case R.id.checkBox3:
+                if (checked) {
+                    Melissa = "Melissa";
+                } else {
+                    Melissa = "";
+                }
+                break;
+            case R.id.checkBox4:
+                if (checked) {
+                    Emmanuel = "Emmanuel";
+                } else {
+                    Emmanuel = "";
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
 }
 
 

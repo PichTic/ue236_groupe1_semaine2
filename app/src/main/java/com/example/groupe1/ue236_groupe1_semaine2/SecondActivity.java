1a249d9e861c0c77195090ebbbf70162a012179c
Noiramir <shadowyzz@hotmail.fr>
2017-11-16 16:19:31 +0100

    Suppression de l'essai précédent

--- app/src/main/java/com/example/groupe1/ue236_groupe1_semaine2/SecondActivity.java
+++ app/src/main/java/com/example/groupe1/ue236_groupe1_semaine2/SecondActivity.java
@@ -245,32 +245,5 @@ http://a-renouard.developpez.com/tutoriels/android/sms/
         Intent backToOldActivity = new Intent(this, MainActivity.class);
         startActivity(backToOldActivity);
     }
-
-    /* Essai pour régler un problème de permission pour l'envoi de sms, mais la méthode checkSelfPermission a été implanté en version 22
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
-    if (ContextCompat.checkSelfPermission(SecondActivity,
-    Manifest.permission.READ_CONTACTS)
-            != PackageManager.PERMISSION_GRANTED) {
-
-        // Should we show an explanation?
-        if (ActivityCompat.shouldShowRequestPermissionRationale(SecondActivity,
-                Manifest.permission.READ_CONTACTS)) {
-
-            // Show an explanation to the user *asynchronously* -- don't block
-            // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
-
-        } else {
-
-            // No explanation needed, we can request the permission.
-
-            ActivityCompat.requestPermissions(SecondActivity,
-                    new String[]{Manifest.permission.READ_CONTACTS},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-        }
-    }*/
+    
 }
